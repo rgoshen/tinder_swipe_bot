6874f6c4e9c7be85cc0aee4ec2c4113debbdf19e
rgoshen <rick.goshen@gmail.com>
2020-12-24 09:16:57 -0700

    cleaned up code

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 from time import sleep
 
-FB_EMAIL = YOUR FACEBOOK LOGIN EMAIL
-FB_PASSWORD = YOUR FACEBOOK PASSWORD
+FB_EMAIL = "YOUR FACEBOOK LOGIN EMAIL"
+FB_PASSWORD = "YOUR FACEBOOK PASSWORD"
 
-chrome_driver_path = YOUR CHROME DRIVER PATH
+chrome_driver_path = "YOUR CHROME DRIVER PATH"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("http://www.tinder.com")
 
 sleep(2)
-login_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
+login_button = driver.find_element_by_xpath(
+    '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
 login_button.click()
 
 sleep(2)
@@ -43,10 +44,10 @@ notifications_button.click()
 cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
 cookies.click()
 
-#Tinder free tier only allows 100 "Likes" per day. If you have a premium account, feel free to change to a while loop.
+# Tinder free tier only allows 100 "Likes" per day. If you have a premium account, feel free to change to a while loop.
 for n in range(100):
 
-    #Add a 1 second delay between likes.
+    # Add a 1 second delay between likes.
     sleep(1)
 
     try:
@@ -55,13 +56,13 @@ for n in range(100):
             '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
         like_button.click()
 
-    #Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
+    # Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
     except ElementClickInterceptedException:
         try:
             match_popup = driver.find_element_by_css_selector(".itsAMatch a")
             match_popup.click()
 
-        #Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
+        # Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
         except NoSuchElementException:
             sleep(2)
 
